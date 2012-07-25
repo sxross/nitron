163b997eac793be17d2a676e415f9e2fb13808b8
Steve Ross <sxross@gmail.com>
2012-07-24 22:39:12 -0700

    Added a comment to clarify how to change the name of the main storyboard.

--- lib/nitron/ui/extensions/app_delegate+storyboard.rb
+++ lib/nitron/ui/extensions/app_delegate+storyboard.rb
@@ -12,6 +12,9 @@ class AppDelegate
     true
   end
 
+  # Override AppDelegate#storyboard to substitute your own storyboard name,
+  # particularly in the case where you have a different one for
+  # the iPhone or the iPad.
   def storyboard
     @storyboard ||= UIStoryboard.storyboardWithName("MainStoryboard", bundle:nil)
   end
