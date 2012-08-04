77ac470735a53a7cc04b5b8c0250868ba459ff24
Steve Ross <sxross@gmail.com>
2012-08-04 13:48:07 -0700

    Added refresh method to allow for refreshing data-bound controls that are not aware of changes. Refactored binding out of viewDidLoad to share between that and refresh.

--- lib/nitron/ui/data_binding_support.rb
+++ lib/nitron/ui/data_binding_support.rb
@@ -26,6 +26,42 @@ module UI
     def viewDidLoad
       super
 
+      perform_bind
+    end
+
+    # When mixed into a view, allows you to refresh it when
+    # an value is suspected to have changed. For e.g.:
+    #
+    #    class YourView < Nitron::ViewController
+    #     def prepareForSegue(segue, sender:sender)
+    #       segue.destinationViewController.delegate = self
+    #     end
+    #    end
+    #
+    #    class ViewYouAreSeguingTo
+    #      attr_accessor :delegate
+    #
+    #      on :save do
+    #        # Stuff preparing for save
+    # 
+    #        model.save!
+    # 
+    #        delegate.refresh if delegate.respond_to?(:refresh)
+    #        close
+    #      end
+    #    end
+    #
+    # because +refresh+ is a method on data-bound views,
+    # simply setting the delegate in your "parent" view
+    # allow you to call refresh through the delegate from
+    # the child.
+    
+    def refresh
+      perform_bind
+    end
+    
+    protected
+    def perform_bind
       @_bindings = DataBinder.shared.bind(model, view)
     end
   end
